38652ea833023af987b0e959edb40ade7b22aaa7
Harsh V Singh <harshv.singh@outlook.com>
2021-05-09 23:30:26 +0530

    Course 1 Week 2 - Assignments 1 to 5 of 8

--- Course1/Week2/fibonacci_huge.py
+++ Course1/Week2/fibonacci_huge.py
@@ -1,19 +1,41 @@
 # Uses python3
 import sys
+import numpy as np
+from numpy.core.numeric import full
 
 def get_fibonacci_huge_naive(n, m):
     if n <= 1:
         return n
-
     previous = 0
     current  = 1
-
     for _ in range(n - 1):
         previous, current = current, previous + current
-
     return current % m
 
+def get_fibonacci_mod(n, m):
+    fib_arr = []
+    mod_arr = []
+    full_pattern = False
+    for i in range(n + 1):
+        fib = i if (i <= 1) else (fib_arr[i - 1] + fib_arr[i - 2])
+        mod = fib % m
+        fib_arr.append(fib)
+        mod_arr.append(mod)
+        if (i >= 2) & (mod_arr[i - 1] == 0) & (mod_arr[i] == 1):
+            full_pattern = True
+            break
+    
+    fib_mod = 0
+    if full_pattern:
+        mod_arr.pop()
+        mod_arr.pop()
+        pos = n % len(mod_arr)
+        fib_mod = mod_arr[pos]
+    else:
+        fib_mod = fib_arr[len(fib_arr) - 1] % m
+    return (fib_mod)
+
 if __name__ == '__main__':
-    input = sys.stdin.read();
+    input = sys.stdin.readline();
     n, m = map(int, input.split())
-    print(get_fibonacci_huge_naive(n, m))
+    print(get_fibonacci_mod(n, m))
--- Course1/Week2/lcm.py
+++ Course1/Week2/lcm.py
@@ -5,11 +5,17 @@ def lcm_naive(a, b):
     for l in range(1, a*b + 1):
         if l % a == 0 and l % b == 0:
             return l
+    return a*b
 
+def lcm_fast(a, b):
+    x = max(a, b)
+    for l in range(x, a*b + 1, x):
+        if l % a == 0 and l % b == 0:
+            return l
     return a*b
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
+    input = sys.stdin.readline()
     a, b = map(int, input.split())
-    print(lcm_naive(a, b))
+    print(lcm_fast(a, b))
 

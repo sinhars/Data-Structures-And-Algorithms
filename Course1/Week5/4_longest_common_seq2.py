3e59e1facd3768e18e5ff2cc3e99f6063e0a89d4
Harsh V Singh <harshv.singh@outlook.com>
2021-05-19 00:10:11 +0530

    Course 1 Week 5 Final assignments

--- Course1/Week5/4_longest_common_seq2.py
+++ Course1/Week5/4_longest_common_seq2.py
@@ -3,8 +3,14 @@
 import sys
 
 def lcs2(a, b):
-    #write your code here
-    return min(len(a), len(b))
+    d = [[0 for i in range(len(b) + 1)] for j in range(len(a) + 1)]
+    for i in range(1, len(d)):
+        for j in range(1, len(d[0])):
+            is_same = 1 if (a[i - 1] == b[j - 1]) else 0 
+            d[i][j] = max(d[i-1][j], d[i][j-1], d[i-1][j-1] + is_same)
+    # for i in range(len(d)):
+    #     print(d[i][:])
+    return (d[-1][-1])
 
 if __name__ == '__main__':
     input = sys.stdin.read()
--- Course1/Week5/5_longest_common_seq3.py
+++ Course1/Week5/5_longest_common_seq3.py
@@ -3,8 +3,14 @@
 import sys
 
 def lcs3(a, b, c):
-    #write your code here
-    return min(len(a), len(b), len(c))
+    d = [[[0 for i in range(len(c) + 1)] for i in range(len(b) + 1)] for j in range(len(a) + 1)]
+    for i in range(1, len(d)):
+        for j in range(1, len(d[0])):
+            for k in range(1, len(d[0][0])):
+                is_same = 1 if (a[i - 1] == b[j - 1] == c[k - 1]) else 0 
+                d[i][j][k] = max(d[i-1][j][k], d[i][j-1][k], d[i][j][k-1], \
+                    d[i-1][j-1][k], d[i-1][j][k-1], d[i][j-1][k-1], d[i-1][j-1][k-1] + is_same)
+    return (d[-1][-1][-1])
 
 if __name__ == '__main__':
     input = sys.stdin.read()

9b0cadb8a9fbc144ec285d28733574db6c529829
Harsh V Singh <harshv.singh@outlook.com>
2021-05-18 16:15:51 +0530

    Course 1 Week 5 Assignments 1, 2 of 5

--- Course1/Week5/1_money_change_dp.py
+++ Course1/Week5/1_money_change_dp.py
@@ -1,10 +1,42 @@
 # Uses python3
 import sys
+import math
 
-def get_change(m):
-    #write your code here
-    return m // 4
+def get_change_fast(money, coins):
+    coin_count = [0] * money
+    for c in range(len(coin_count)):
+        min_count = math.inf
+        curr_money = c + 1
+        for coin in coins:
+            if curr_money == coin:
+                curr_count = 1
+            elif curr_money > coin:
+                prev_money = curr_money - coin
+                curr_count = coin_count[prev_money - 1] + 1
+            else:
+                curr_count = math.inf
+            if curr_count < min_count:
+                min_count = curr_count
+        coin_count[c] = min_count
+    
+    return (coin_count[money - 1])
+
+def get_change(money, coins):
+    coin_count = 0
+    if money <= 0:
+        coin_count = 0
+    elif money in coins:
+        coin_count = 1
+    else:
+        coin_count = math.inf
+        for coin in coins:
+            if money > coin:
+                money_left = money - coin
+                coins_left = get_change(money=money_left, coins=coins)
+                coin_count = min(coin_count, coins_left + 1)
+    return coin_count
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
-    print(get_change(m))
+    money = int(sys.stdin.readline())
+    coins = [1, 3, 4]
+    print(get_change_fast(money=money, coins=coins))
--- Course1/Week5/2_primitive_calculator.py
+++ Course1/Week5/2_primitive_calculator.py
@@ -1,7 +1,42 @@
 # Uses python3
 import sys
 
+def fast_optimal_sequence(n):
+    sequences = [0] * n
+    for i in range(len(sequences)):
+        if i == 0:
+            sequences[i] = [1]
+        else:
+            prev_best = sequences[i - 1]
+            if ((i + 1) % 2 == 0):
+                option2 = sequences[((i + 1) // 2) - 1] 
+                if len(option2) < len(prev_best):
+                    prev_best = option2
+            if ((i + 1) % 3 == 0):
+                option3 = sequences[((i + 1) // 3) - 1] 
+                if len(option3) < len(prev_best):
+                    prev_best = option3
+            curr_seq = prev_best + [i + 1]
+            sequences[i] = curr_seq
+    return (sequences[-1])
+
 def optimal_sequence(n):
+    if n == 1:
+        return [1]
+    else:
+        prev_best = optimal_sequence(n - 1)
+        if n % 3 == 0:
+            prev_seq_3 = optimal_sequence(n // 3)
+            if len(prev_seq_3) < len(prev_best):
+                prev_best = prev_seq_3
+        if n % 2 == 0:
+            prev_seq_2 = optimal_sequence(n // 2)
+            if len(prev_seq_2) < len(prev_best):
+                prev_best = prev_seq_2
+        prev_best.append(n)
+        return (prev_best)
+
+def greedy_sequence(n):
     sequence = []
     while n >= 1:
         sequence.append(n)
@@ -13,9 +48,10 @@ def optimal_sequence(n):
             n = n - 1
     return reversed(sequence)
 
-input = sys.stdin.read()
-n = int(input)
-sequence = list(optimal_sequence(n))
-print(len(sequence) - 1)
-for x in sequence:
-    print(x, end=' ')
+if __name__ == '__main__':
+    input = sys.stdin.readline()
+    n = int(input)
+    sequence = list(fast_optimal_sequence(n))
+    print(len(sequence) - 1)
+    for x in sequence:
+        print(x, end=' ')

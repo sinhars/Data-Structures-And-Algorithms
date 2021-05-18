1675eb2ca816930a0e4b684a7113250043f1c210
Harsh V Singh <harshv.singh@outlook.com>
2021-05-18 23:41:42 +0530

    Course 1 Week 5 Assignment 3 of 5

--- Course1/Week5/3_edit_distance.py
+++ Course1/Week5/3_edit_distance.py
@@ -1,7 +1,20 @@
 # Uses python3
+
 def edit_distance(s, t):
-    #write your code here
-    return 0
+    d = [[0 for i in range(len(t) + 1)] for j in range(len(s) + 1)]
+    for i in range(len(d)):
+        d[i][0] = i
+    for j in range(len(d[0])):
+        d[0][j] = j
+    for i in range(1, len(d)):
+        for j in range(1, len(d[0])):
+            diag = 0 if (s[i - 1] == t[j - 1]) else 1
+            d[i][j] = min(d[i - 1][j] + 1, d[i][j - 1] + 1, d[i - 1][j - 1] + diag)
+    
+    # for i in range(len(d)):
+    #     print(d[i][:])
+    
+    return (d[-1][-1])
 
 if __name__ == "__main__":
     print(edit_distance(input(), input()))

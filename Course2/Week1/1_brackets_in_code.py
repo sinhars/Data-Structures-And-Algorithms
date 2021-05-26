9200e42813ed9c28e12f817b938a4577bee0f513
Harsh V Singh <harshv.singh@outlook.com>
2021-05-26 13:18:35 +0530

    Course 2 Week 1 Assigment 1 of 5

---
+++ Course2/Week1/1_brackets_in_code.py
@@ -0,0 +1,36 @@
+# python3
+
+from collections import namedtuple
+
+Bracket = namedtuple("Bracket", ["char", "position"])
+
+def are_matching(left, right):
+    return (left + right) in ["()", "[]", "{}"]
+
+def find_mismatch(text):
+    opening_brackets_stack = []
+    for i, next in enumerate(text):
+        if next in "([{":
+            opening_brackets_stack.append(Bracket(next, i))
+            pass
+
+        if next in ")]}":
+            if len(opening_brackets_stack) > 0:
+                prev = opening_brackets_stack[len(opening_brackets_stack) - 1]
+                if are_matching(prev.char, next):
+                    opening_brackets_stack.pop()
+                else:
+                    return (i + 1)
+            else:
+                return(i + 1)
+            pass
+    
+    if len(opening_brackets_stack) > 0:
+        return (len(opening_brackets_stack))
+
+    return ("Success")
+
+if __name__ == "__main__":
+    text = input()
+    mismatch = find_mismatch(text)
+    print(mismatch)

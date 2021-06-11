92f42ac347a69260a28cfb37d3013bfe9b045448
Harsh V Singh <harshv.singh@outlook.com>
2021-06-11 17:45:32 +0530

    Course 2 Week 1 Assignments 4 of 6

--- Course2/Week1/4_stack_with_max.py
+++ Course2/Week1/4_stack_with_max.py
@@ -4,17 +4,23 @@ import sys
 class StackWithMax():
     def __init__(self):
         self.__stack = []
+        self.__max_stack = []
 
     def Push(self, a):
         self.__stack.append(a)
+        curr_max = a if (len(self.__max_stack) == 0) else self.__max_stack[len(self.__max_stack) - 1]
+        if a >= curr_max:
+            self.__max_stack.append(a)
 
     def Pop(self):
         assert(len(self.__stack))
+        if self.__stack[len(self.__stack) - 1] == self.__max_stack[len(self.__max_stack) - 1]:
+            self.__max_stack.pop()
         self.__stack.pop()
 
     def Max(self):
         assert(len(self.__stack))
-        return max(self.__stack)
+        return (self.__max_stack[len(self.__max_stack) - 1])
 
 
 if __name__ == '__main__':
